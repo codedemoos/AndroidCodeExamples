8d21549bada8ab185c5207e7caae22134720656e
codedemoos <c>
2014-02-08 16:01:45 +0100

    OK

--- AppDemo/src/com/codedemoos/android/MainActivity.java
+++ AppDemo/src/com/codedemoos/android/MainActivity.java
@@ -1,22 +1,83 @@
 package com.codedemoos.android;
 
+
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+
 
 public class MainActivity extends Activity {
 
+	
+	LinearLayout mainLinearLayout;
+	
+	ScrollView mainScrollView; 
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
+		
+		
+		
+		//CREATE COMMON LAYOUT PARAMS
+		LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+		//GET THE MAIN SCROLL VIEW
+		
+		mainScrollView = (ScrollView) findViewById(R.id.mainScrollView);
+		
+		//GET THE MAIN LINEAR LAYOUT
+		
+		mainLinearLayout = (LinearLayout) findViewById(R.id.mainLinearLayout);
+				
+	
+		//CREATE A BUTTON 
+		Button buttonDemoToast = new Button(this);		
+		//SET TEXT
+		buttonDemoToast.setText("Demo Toast");		
+		//SET LAYOUT PARAMS
+		buttonDemoToast.setLayoutParams(p);		
+		//SET CLICK LISTENER
+		buttonDemoToast.setOnClickListener(buttonDemoToastClickListener);		
+		//ADD VIEW
+		mainLinearLayout.addView(buttonDemoToast);
+		
+	
+		
+	
+		
+		
+		
+
+
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	
+
+	
+      
+  	//buttonDemoToast - CLICK LISTENER
+      private OnClickListener buttonDemoToastClickListener= new OnClickListener() { 	
+          public void onClick(View v) {       	
+          	//simple navigation
+          	Intent intent = new Intent(MainActivity.this,
+          			ActivityDemoToast.class);
+  			startActivity(intent); 			
+          	}          
+        };
+	
 
 }
